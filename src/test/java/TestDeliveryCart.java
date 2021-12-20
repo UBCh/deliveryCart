f0201c12937a33750a3aa38e9b50daf304f0c63c
UBCh <u2b127@gmail.ru>
2021-12-20 15:15:24 +0300

    task completed

--- src/test/java/TestDeliveryCart.java
+++ src/test/java/TestDeliveryCart.java
@@ -1,10 +1,13 @@
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -15,20 +18,26 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TestDeliveryCart {
 
-    java.util.Date date = new java.util.Date();
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(4);
 
     @Test
     void shouldSubmittingForm() {
         open("http://localhost:9999");
-        $("[data-test-id='city']").setValue("москва");
-        GetData data = new GetData();
-        $(".calendar-input").val(String.valueOf(data));
-        // $("[data-test-id='date']").setValue("24.12.2021");
-        $("[data-test-id='name']").setValue("Мис Пигги");
-        $("[data-test-id='phone']").setValue("+79095463728");
-        $("[data-test-id='agreement']").click();
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Мис Пигги");
+        $("[data-test-id='phone'] input").setValue("+79095463728");
+        $(".checkbox__text").click();
         $(".button__text").click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofMillis(5000));
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 
 }
